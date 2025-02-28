4f8dc41f38ef696d8ea70ef562572d318f2905e4
McKathlin <mckathlin@gmail.com>
2025-02-28 19:22:04 +0000

    Create survey intro page

---
+++ public_html/index.php
@@ -0,0 +1,39 @@
+<?php
+  // Extract the survey
+  const SURVEY_PATH = "../surveys/favorites.json";
+  $survey = json_decode(file_get_contents(SURVEY_PATH));
+  $title = htmlentities($survey->title);
+  $introParagraphs = htmlentities($survey->intro);
+
+  // Start the session
+  session_start();
+  $_SESSION['questions'] = $survey->questions;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?=$title?></title>
+</head>
+<body>
+  <header>
+    <h1><?=$title?></h1>
+  </header>
+  <main>
+    <p>
+      <?=$introParagraphs?>
+    </p>
+    <nav class="button-row">
+      <form action="./survey.php" method="post">
+        <button type="submit" name="qNum" value="1">Start the Survey</button>
+      </form>
+    </nav>
+  </main>
+  <footer>
+    <p>
+      Made by <a href="https://github.com/mckathlin">McKathlin</a>
+    </p>
+  </footer>
+</body>
+</html>
