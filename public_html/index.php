dae286691addc0680cd14f9bae501b2f4e4862fc
McKathlin <mckathlin@gmail.com>
2025-03-01 15:13:36 +0000

    Group all constant survey info in one session object

--- public_html/index.php
+++ public_html/index.php
@@ -4,13 +4,11 @@
   // Extract the survey
   const SURVEY_PATH = "../surveys/favorites.json";
   $survey = json_decode(file_get_contents(SURVEY_PATH));
-  $title = htmlentities($survey->title);
   $introParagraphs = text_to_paragraphs($survey->intro);
 
   // Start the session
   session_start();
-  $_SESSION['title'] = $title;
-  $_SESSION['questions'] = $survey->questions;
+  $_SESSION['survey'] = $survey;
   $_SESSION['answers'] = [];
 ?>
 <!DOCTYPE html>
@@ -18,11 +16,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?=$title?></title>
+  <title><?=$survey->title?></title>
 </head>
 <body>
   <header>
-    <h1><?=$title?></h1>
+    <h1><?=$survey->title?></h1>
   </header>
   <main>
     <?=$introParagraphs?>
--- public_html/survey.php
+++ public_html/survey.php
@@ -1,11 +1,11 @@
 <?php
   // Resume the session.
   session_start();
-  if (!isset($_SESSION['questions'])) {
+  if (!isset($_SESSION['survey'])) {
     header("Location: /index.php");
   }
-  $title = $_SESSION['title'];
-  $allQuestions = $_SESSION['questions'];
+  $survey = $_SESSION['survey'];
+  $questionCount = count($survey->questions);
 
   // Store the previous answer on the session
   $previousIndex = intval($_POST['index'] ?? 0);
@@ -13,8 +13,7 @@
 
   // Find the current question.
   $questionIndex = $previousIndex + intval($_POST['direction']);
-  $questionCount = count($allQuestions);
-  $question = $allQuestions[$questionIndex];
+  $question = $survey->questions[$questionIndex];
 
   // If we're passing the last question, proceed to the review page.
   if ($questionIndex >= $questionCount) {
@@ -26,11 +25,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?=$title?></title>
+  <title><?=$survey->title?></title>
 </head>
 <body>
   <header>
-    <h1><?=$title?></h1>
+    <h1><?=$survey->title?></h1>
     <p><?=var_dump($_POST)?></p>
   </header>
   <main>
