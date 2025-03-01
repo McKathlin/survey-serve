895c5737a0fed7ea3f72ca63f8b15d3af4638ac5
McKathlin <mckathlin@gmail.com>
2025-03-01 16:47:10 +0000

    Display questions and answers in review.php

--- public_html/review.php
+++ public_html/review.php
@@ -1,16 +1,32 @@
+<?php
+require '../survey_session.php';
+require '../lib/html_helper.php';
+
+$survey = SurveySession::resume();
+if (is_null($survey)) {
+  header("Location: /index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SurveyName</title>
+  <title><?=$survey->title?></title>
 </head>
 <body>
   <header>
-    <h1>SurveyName</h1>
+    <h1><?=$survey->title?></h1>
   </header>
   <main>
-    <p>Questions will be reviewed here.</p>
+    <h2>Thank you! Your answers are below:</h2>
+    <?php for($i = 0; $i < $survey->questionCount(); $i++) { ?>
+      <section>
+        <h3>Question <?=($i + 1)?></h3>
+        <p class="question"><?=$survey->questions[$i]->text?></p>
+        <p class="answer"><?=$survey->answers[$i]?></p>
+      </section>
+    <?php } /* end for $i */ ?>
   </main>
   <footer>
     <p>
