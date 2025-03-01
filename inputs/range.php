72833d2e470dcfe3fc6d2e281855f56991f45cd3
McKathlin <mckathlin@gmail.com>
2025-03-01 23:30:29 +0000

    Add range input type

---
+++ inputs/range.php
@@ -0,0 +1,10 @@
+<label for="answer">
+  <p><?=$question->text?></p>
+  <p class="instruction">Adjust the sliding scale below to show your answer.</p>
+  <div class="range-container">
+    <span class="min-label"><?=$question->minLabel?></span>
+    <input type="range" id="answer" name="answer"
+      min="<?=$question->min?>" max="<?=$question->max?>">
+    <span class="max-label"><?=$question->maxLabel?></span>
+  </div>
+</label>
